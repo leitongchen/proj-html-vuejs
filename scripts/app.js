4cae2a774c704cabfbdd45589596e5e8d3af97a0
leitong <leitongchen93@gmail.com>
2021-05-26 09:43:16 +0200

    Reorganizing data in data.js

--- scripts/app.js
+++ scripts/app.js
@@ -16,23 +16,31 @@ const app = new Vue({
                 text: "departments",
                 link: "#",
                 type: "link",
-                childs: [
+                childs: true,
+                ddmenu: departmentsList,
+                /*
+                ddmenu: [
                     {
-                        text: "general practice",
+                        text: "Cardiology",
                         link: "#",
-                        type: "link",
                     },
                     {
-                        text: "cardiology",
+                        text: "Pediatrics",
                         link: "#",
-                        type: "link",
                     },
                     {
-                        text: "pediatrics",
+                        text: "Diabets Care",
                         link: "#",
-                        type: "link",
                     },
-                ]
+                    {
+                        text: "Pre-Natal Care",
+                        link: "#",
+                    },
+                    {
+                        text: "Ultrasound Echocardiogram",
+                        link: "#",
+                    },
+                ] */
             },
             {
                 text: "articles",
@@ -45,174 +53,31 @@ const app = new Vue({
                 type: "btn",
             },            
         ],
-        ourDoctors: [
-            {
-                name: "Jon Snow",
-                department: "Anesthesiologist",
-                description: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque.",
-                photo: "doctor-1.jpg",
-                socialLinks: [
-                    {
-                        text: "facebook-f", 
-                        link: "#"
-                    },
-                    {
-                        text: "twitter", 
-                        link: "#"
-                    },
-                    {
-                        text: "instagram", 
-                        link: "#"
-                    },
-                ]
-            },
-            {
-                name: "Toni Stark",
-                department: "Cardiologist",
-                description: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque.",
-                photo: "doctor-2.jpg",
-                socialLinks: [
-                    {
-                        text: "facebook-f", 
-                        link: "#"
-                    },
-                    {
-                        text: "twitter", 
-                        link: "#"
-                    },
-                    {
-                        text: "instagram", 
-                        link: "#"
-                    },
-                ]
-            },
-            {
-                name: "Anna Smith",
-                department: "Nurse Practitioner",
-                description: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque.",
-                photo: "doctor-3.jpg",
-                socialLinks: [
-                    {
-                        text: "facebook-f", 
-                        link: "#"
-                    },
-                    {
-                        text: "twitter", 
-                        link: "#"
-                    },
-                    {
-                        text: "instagram", 
-                        link: "#"
-                    },
-                ]
-            },
-        ],
-        healthServices: [
-            {
-                icon: "14.png",
-                name: "Rehabilitation Center",
-                description: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas.",
-            },
-            {
-                icon: "15.png",
-                name: "Dental Implants",
-                description: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas.",
-            },
-            {
-                icon: "16.png",
-                name: "Medicine Research",
-                description: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas.",
-            },
-            {
-                icon: "11.png",
-                name: "Blood Bank",
-                description: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas.",
-            },
-            {
-                icon: "12.png",
-                name: "Pharmaceutical advice",
-                description: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas.",
-            },
-            {
-                icon: "13.png",
-                name: "Medical Counseling",
-                description: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas.",
-            },
-        ],
-        clientsList: [
-            {
-                name: "Healthy",
-                logo: "-1.png"
-            }, 
-            {
-                name: "Health",
-                logo: "-2.png"
-            }, 
-            {
-                name: "Turbo Power",
-                logo: "-3.png"
-            }, 
-            {
-                name: "Color Power",
-                logo: "-4.png"
-            }, 
-        ],
-        socialLinkList: [
-            {
-                text: "facebook-f", 
-                link: "www.facebook.com"
-            }, 
-            {
-                text: "twitter", 
-                link: "www.twitter.com"
-            }, 
-            {
-                text: "youtube", 
-                link: "www.youtube.com"
-            }, 
-            {
-                text: "instagram", 
-                link: "www.instagram.com"
-            }, 
-        ],
-        departmentsList: [
-            {
-                name: "Cardiology",
-                link: "#",
-            },
-            {
-                name: "Pediatrics",
-                link: "#",
-            },
-            {
-                name: "Diabets Care",
-                link: "#",
-            },
-            {
-                name: "Pre-Natal Care",
-                link: "#",
-            },
-            {
-                name: "Ultrasound Echocardiogram",
-                link: "#",
-            },
-        ],
+        ourDoctors: ourDoctors,
+        healthServices: healthServices,
+        clientsList: clientsList,
+        socialLinkList: socialLinkList,
+        departmentsList: departmentsList,
 
-        childMenu: false,
+        ddMenuOpen: false,
 
+    },
+    computed: {
+     
     },
     methods: {
         onLinkClick(event) {
             console.log("apre")
-            this.childMenu = !this.childMenu;
-            if (this.childMenu) {
+
+            this.ddMenuOpen = !this.ddMenuOpen;
+            if (this.ddMenuOpen) {
                 event.currentTarget.focus()
             }
         },
         focusLost(event) {
             console.log("chiude")
             
-            this.childMenu = false; 
+            this.ddMenuOpen = false; 
             
         }
     }
---
+++ scripts/data.js
@@ -0,0 +1,156 @@
+const ourDoctors = [
+    {
+        name: "Jon Snow",
+        department: "Anesthesiologist",
+        description: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque.",
+        photo: "doctor-1.jpg",
+        socialLinks: [
+            {
+                text: "facebook-f", 
+                link: "http://www.facebook.com"
+            },
+            {
+                text: "twitter", 
+                link: "#"
+            },
+            {
+                text: "instagram", 
+                link: "#"
+            },
+        ]
+    },
+    {
+        name: "Toni Stark",
+        department: "Cardiologist",
+        description: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque.",
+        photo: "doctor-2.jpg",
+        socialLinks: [
+            {
+                text: "facebook-f", 
+                link: "#"
+            },
+            {
+                text: "twitter", 
+                link: "#"
+            },
+            {
+                text: "instagram", 
+                link: "#"
+            },
+        ]
+    },
+    {
+        name: "Anna Smith",
+        department: "Nurse Practitioner",
+        description: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque.",
+        photo: "doctor-3.jpg",
+        socialLinks: [
+            {
+                text: "facebook-f", 
+                link: "#"
+            },
+            {
+                text: "twitter", 
+                link: "#"
+            },
+            {
+                text: "instagram", 
+                link: "#"
+            },
+        ]
+    },
+];
+
+const healthServices =[
+    {
+        icon: "14.png",
+        name: "Rehabilitation Center",
+        description: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas.",
+    },
+    {
+        icon: "15.png",
+        name: "Dental Implants",
+        description: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas.",
+    },
+    {
+        icon: "16.png",
+        name: "Medicine Research",
+        description: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas.",
+    },
+    {
+        icon: "11.png",
+        name: "Blood Bank",
+        description: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas.",
+    },
+    {
+        icon: "12.png",
+        name: "Pharmaceutical advice",
+        description: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas.",
+    },
+    {
+        icon: "13.png",
+        name: "Medical Counseling",
+        description: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas.",
+    },
+];
+
+const clientsList = [
+    {
+        name: "Healthy",
+        logo: "-1.png"
+    }, 
+    {
+        name: "Health",
+        logo: "-2.png"
+    }, 
+    {
+        name: "Turbo Power",
+        logo: "-3.png"
+    }, 
+    {
+        name: "Color Power",
+        logo: "-4.png"
+    }, 
+];
+
+const socialLinkList = [
+    {
+        text: "facebook-f", 
+        link: "https://www.facebook.com/"
+    }, 
+    {
+        text: "twitter", 
+        link: "https://www.twitter.com"
+    }, 
+    {
+        text: "youtube", 
+        link: "https://www.youtube.com"
+    }, 
+    {
+        text: "instagram", 
+        link: "https://www.instagram.com"
+    }, 
+];
+
+const departmentsList = [
+    {
+        text: "Cardiology",
+        link: "#",
+    },
+    {
+        text: "Pediatrics",
+        link: "#",
+    },
+    {
+        text: "Diabets Care",
+        link: "#",
+    },
+    {
+        text: "Pre-Natal Care",
+        link: "#",
+    },
+    {
+        text: "Ultrasound Echocardiogram",
+        link: "#",
+    },
+];
